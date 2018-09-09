364d8d85bf9dc7c1b0da6f24242de601f1e1251c
Emil Jansson <ejansson.swe@gmail.com>
2018-09-09 15:39:54 +0200

    Revert "Minified again"
    This reverts commit f0a562f629da566fa8a59693e5e6db60a004b64a.

--- js/validate.js
+++ js/validate.js
@@ -1,1 +1,57 @@
-$(document).ready(function(){$("#hire-form").validate({rules:{name:"required",email:{required:!0,email:!0},subject:"required",message:{required:!0,minlength:10,maxlength:500},accept:"required"},messages:{name:"Please write your name!",email:{required:"Enter your email address",email:"Please enter a valid email"},subject:"",message:{required:"Please, write something about the project",minlength:"You need to enter at least 10 characters",maxlength:"You can max enter 500 characters"},accept:""},submitHandler:function(e){$(e).ajaxSubmit()}})}),$("#clearform").on("click",function(){$("#hire-form").validate().resetForm(),$("#feedback").html("")});
+//Validates the form and if validation is succeeded it runs a ajax request
+$(document).ready(function() {
+  $('#hire-form').validate({
+    rules: {
+      name: "required",
+      email: {
+        required: true,
+        email: true,
+      },
+      subject: "required",
+			message: {
+				required: true,
+				minlength: 10,
+        maxlength: 500,
+			},
+      accept: "required"
+		},
+		messages: {
+			name: "Please write your name!",
+			email: {
+				required: "Enter your email address",
+				email: "Please enter a valid email"
+      },
+      subject: "",
+			message: {
+				required: "Please, write something about the project",
+				minlength: "You need to enter at least 10 characters",
+        maxlength: "You can max enter 500 characters"
+			},
+      accept: ""
+    },
+    submitHandler: function(form) {
+      $(form).ajaxSubmit();
+    //   $.ajax({
+    //     url: "https://formspree.io/e.jansson@icloud.com",
+    //     type: "POST",
+    //     data: {message: "hello!"},
+    //     dataType: "json",
+    //     // success: function(response) {
+    //     //   $("#feedback").html("<p>Thank you for submitting!</p>");
+    //     // },
+    //     // error: function(jqXHR, textStatus, errorThrown) {
+    //     //   console.log(textStatus, errorThrown);
+    //     //   $("#feedback").html("<p>There was an error</p>");
+    //     // }
+    // });
+    //  event.preventDefault();
+    }
+  });
+});
+
+//Adds functionality to the reset button
+$('#clearform').on('click', function () {
+    $("#hire-form").validate().resetForm();
+    $("#feedback").html('');
+
+ });
